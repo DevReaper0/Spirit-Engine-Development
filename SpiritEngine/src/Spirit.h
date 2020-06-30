1f777069fde6124dc5deb01950e1ee7b6287835c
DaRubyMiner360 <55484201+DaRubyMiner360@users.noreply.github.com>
2020-06-30 12:36:33 -0500

    Orginization and Start Again

---
+++ SpiritEngine/src/Spirit.h
@@ -0,0 +1,22 @@
+#pragma once
+
+// For use by Spirit Applications
+
+// ---Spirit Engine-----------------------
+#include "Spirit/Application.h"
+//#include "Spirit/Log.h"
+#include "Spirit/maths/maths.h"
+// ---------------------------------------
+
+// ---Entry Point-------------------------
+#include "Spirit/EntryPoint.h"
+// ---------------------------------------
+
+// ---Non-Spirit-------------------------
+// ---------Pre-Compiled Headers---------------
+#include "spiritpch.h"
+// --------------------------------------------
+
+// ---------Libraries--------------------------
+// --------------------------------------------
+// --------------------------------------
---
+++ SpiritEngine/src/spiritpch.cpp
@@ -0,0 +1,2 @@
+#include "spiritpch.h"
+
